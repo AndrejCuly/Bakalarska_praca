408cbd57c2ebb3f879fd660d5fc5b78b75c7bbd4
Andrej Čulý <andrejculyskola@gmail.com>
2026-03-20 00:54:24 +0100

    Add 3d_classification_whole_mamm into folder

---
+++ 3d_classification_whole_mamm/test_dataset.py
@@ -0,0 +1,22 @@
+from dataset3d import Mammogram3DDataset
+
+dataset = Mammogram3DDataset(
+    r"C:\Users\culya\Desktop\data_bakalarka\data\dataset_processed\train"
+)
+
+count_positive = 0
+
+for i in range(200):
+
+    img, mask = dataset[i]
+
+    if mask.sum() > 0:
+        count_positive += 1
+
+print("positive samples:", count_positive)
+
+img, mask = dataset[0]
+
+print(img.shape)
+print(mask.shape)
+print("mask sum:", mask.sum())
